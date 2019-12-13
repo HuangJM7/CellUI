138467b01f2f3bcd9d7b06a638a6a83a5dbeae86
HuangJM7 <jm_william@qq.com>
2019-12-13 16:33:48 +0800

    单元测试
    创建vue实例,输入配置,断言输出
    测试CSS属性时,需要添加到页面
    测试完成后销毁页面元素及vue实例对象
    测试监听事件时,遇到bug

--- src/app.js
+++ src/app.js
@@ -13,3 +13,94 @@ new Vue({
         loading1: true,
     }
 })
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect
+
+//测试输入icon配置属性时,断言输出
+{
+    const Constructor = Vue.extend(Button)//获取button组件构造函数
+    const vm = new Constructor({//
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-settings')
+    vm.$el.remove()//删除页面元素
+    vm.$destroy()//删除实例对象
+}
+//测试输入icon及loading配置属性,断言输出
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#i-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+//测试默认情况下icon的order
+{
+    //测试CSS属性,元素需要添加到页面中
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    })
+    vm.$mount(div)//挂载到页面中,CSS属性才有值
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)//解构赋值,获取svg对象的order属性
+    expect(order).to.eq('1')//CSS属性值为字符串
+    vm.$el.remove()
+    vm.$destroy()
+}
+//测试输入iconPosition属性,断言输出icon的order为2
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            iconPosition: 'right'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('2')
+    vm.$el.remove()
+    vm.$destroy()
+}
+//测试button是否监听click事件
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+        }
+    })
+    vm.$mount()
+
+    //实例监听click事件,回调函数中定义错误断言,当错误提示出现,说明监听到click事件
+    //无法做到监听事件成功,断言成功,使用mock(chai.spy)进行优化
+    vm.$on('click', function () {
+        expect(1).to.eq(2)
+    })
+
+    let button = vm.$el
+    button.click()//触发button元素的click事件
+}
