d18ecf0ae818f807f6a2cfb54bb28a9e8ffe1e1f
HuangJM7 <jm_william@qq.com>
2019-12-16 17:02:46 +0800

    API offset设置间隔

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
+  <div
+    class="col"
+    :class="[span ? `col-${span}` : '', offset ? `offset-${offset}` : '']"
+  >
     <slot></slot>
   </div>
 </template>
@@ -9,7 +12,10 @@ export default {
   name: "unit-col",
   props: {
     // span: {type: [String, Number]}
-    span: [String, Number] //简写
+    span: [String, Number], //简写
+    offset: {
+      type: [String, Number]
+    }
   }
 };
 </script>
@@ -25,6 +31,12 @@ export default {
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n / 24) * 100%;
+    }
+  }
+  $class-prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n / 24) * 100%;
     }
   }
 }
