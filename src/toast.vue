65c258a203e8ce69d9101b7f75ef65c4de782f8c
HuangJM7 <jm_william@qq.com>
2019-12-19 16:28:37 +0800

    toast 测试

--- src/toast.vue
+++ src/toast.vue
@@ -16,12 +16,11 @@ export default {
   name: "unit-toast",
   props: {
     autoClose: {
-      type: Boolean,
-      default: true
-    },
-    autoCloseDelay: {
-      type: Number,
-      default: 5
+      type: [Boolean, Number],
+      default: 5,
+      validator(value) {
+        return value === false || typeof value === "number";
+      }
     },
     closeButton: {
       type: Object,
@@ -54,6 +53,7 @@ export default {
   methods: {
     close() {
       this.$el.remove();
+      this.$emit("close");
       this.$destroy();
     },
     onClickClose() {
@@ -67,7 +67,7 @@ export default {
       if (this.autoClose) {
         setTimeout(() => {
           this.close();
-        }, this.autoCloseDelay * 1000);
+        }, this.autoClose * 1000);
       }
     },
     updataStyles() {
