4e6ecae3fdd1e6880e064dd54db244174b571bf4
HuangJM7 <jm_william@qq.com>
2019-12-21 01:29:16 +0800

    popover 开关收拢 点击逻辑优化

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click.stop="onClick">
+  <div class="popover" @click.stop="onClick" ref="popover">
     <div class="content" v-if="visible" ref="content">
       <slot name="content"></slot>
     </div>
@@ -18,28 +18,49 @@ export default {
     };
   },
   methods: {
-    onClick() {
-      this.visible = !this.visible;
-
-      if (this.visible === true) {
-        this.$nextTick(() => {
-          document.body.appendChild(this.$refs.content);
-          let {
-            width,
-            height,
-            top,
-            left
-          } = this.$refs.trigger.getBoundingClientRect();
-          this.$refs.content.style.left = left + window.scrollX + "px";
-          this.$refs.content.style.top = top + window.scrollY + "px";
-
-          let hander = () => {
-            this.visible = false;
-            console.log("document");
-            document.removeEventListener("click", hander);
-          };
-          document.addEventListener("click", hander);
-        });
+    open() {
+      //显示div并定位,添加关闭监听
+      this.visible = true;
+      this.$nextTick(() => {
+        this.positionContent();
+        document.addEventListener("click", this.onClickDocument);
+      });
+    },
+    close() {
+      //关闭div,移除关闭监听
+      this.visible = false;
+      document.removeEventListener("click", this.onClickDocument);
+    },
+    positionContent() {
+      //负责div区域的定位
+      document.body.appendChild(this.$refs.content);
+      let {
+        width,
+        height,
+        top,
+        left
+      } = this.$refs.trigger.getBoundingClientRect();
+      this.$refs.content.style.left = left + window.scrollX + "px";
+      this.$refs.content.style.top = top + window.scrollY + "px";
+    },
+    onClickDocument(e) {
+      //负责非popover区域的点击关闭逻辑
+      if (
+        this.$refs.popover === e.target ||
+        this.$refs.popover.contains(e.target)
+      ) {
+        return;
+      }
+      this.close();
+    },
+    onClick(e) {
+      //负责 button 的点击关闭开启逻辑
+      if (this.$refs.trigger.contains(e.target)) {
+        if (this.visible === true) {
+          this.close();
+        } else {
+          this.open();
+        }
       }
     }
   }
