34b15b26f609a4bcfcc93832cb28a1240a9364d6
HuangJM7 <jm_william@qq.com>
2019-12-22 19:57:06 +0800

    single API

--- src/collapse-item.vue
+++ src/collapse-item.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="collapseItem">
-    <div class="title" @click="open = !open">
+    <div class="title" @click="toggle">
       {{ title }}
     </div>
-    <div class="content" v-if="open">
+    <div class="content" v-if="visible">
       <slot></slot>
     </div>
   </div>
@@ -12,6 +12,7 @@
 <script>
 export default {
   name: "unit-collapse-item",
+  inject: ["eventBus"],
   props: {
     title: {
       type: String,
@@ -20,8 +21,32 @@ export default {
   },
   data() {
     return {
-      open: false
+      visible: false
     };
+  },
+  methods: {
+    toggle() {
+      if (this.visible) {
+        this.close();
+      } else {
+        this.open();
+      }
+    },
+    open() {
+      this.visible = true;
+      this.eventBus && this.eventBus.$emit("update:selected", this);
+    },
+    close() {
+      this.visible = false;
+    }
+  },
+  mounted() {
+    this.eventBus &&
+      this.eventBus.$on("update:selected", vm => {
+        if (vm !== this) {
+          this.close();
+        }
+      });
   }
 };
 </script>
@@ -41,6 +66,7 @@ $border-radius: 4px;
     padding: 0 8px;
   }
   &:first-child {
+    //第一个标题上边框圆角
     > .title {
       border-top-left-radius: $border-radius;
       border-top-right-radius: $border-radius;
@@ -48,6 +74,7 @@ $border-radius: 4px;
   }
   &:last-child {
     .title:last-child {
+      //最后一个折叠面板的最后一个元素为title时
       border-bottom: none;
       border-bottom-left-radius: $border-radius;
       border-bottom-right-radius: $border-radius;
